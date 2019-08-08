e34a3581ae024753a4b9cac8d6aaedfa8f792dd5
DreamerYee <1584802414@qq.com>
2019-08-08 18:33:56 +0800

    update DemoB字符串.py

---
+++ python/pro0808/DemoB字符串.py
@@ -0,0 +1,32 @@
+# Author:DreamYee
+# Create:2019/08/08 9:50
+# File:DemoB字符串.py
+
+# 内建函数：input(将内容输入到控制台上)   print
+name = input("What's your name?")
+print("My name is:" + name)
+number = input(5)
+print(number)
+'''
+内建函数：
+|abs() | divmod() | input() | open() |
+staticmethod() |all() | enumerate() | 
+int() | ord() | str() | any() | eval() |
+isinstance() | pow() | sum() | basestring() |
+execfile() | issubclass() | print() | super() |
+bin() | file() | iter() | property() | tuple() |
+bool() | filter() | len() | range() | type() |
+bytearray() | float() | list() | raw_print() |
+unichr() | callable() | format() | locals() |
+reduce() | unicode() | chr() | frozenset() |
+long() |reload() | vars() | classmethod() |
+getattr()| map() | repr() | xrange()|
+|cmp() | globals()| max()| reversed()| zip()|
+|compile() |hasattr() | memoryview()| round() | import()|
+|complex() |hash() | min()| set() | apply()|
+|delattr() |help()| next()| setattr()| buffer()|
+|dict() | hex() |object() |slice() | coerce()|
+|dir() | id() |oct() |sorted() |intern()|
+'''
+# 原始字符串：字符串里面的每个字符都是原始含义，例：反斜杠
+
