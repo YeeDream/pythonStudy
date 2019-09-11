928bca19dcb08e802bce4a72e0ba411f2329dcdb
DreamerYee <1584802414@qq.com>
2019-09-11 21:43:07 +0800

    update DemoB-链表.py

---
+++ python-practice/pro0911/DemoB-链表.py
@@ -0,0 +1,13 @@
+# -*-coding:utf-8 -*-
+# *-*Author:DreamYee*-*
+# Create:2019/09/11 21:35
+# File:DemoB-链表.py
+
+linkTable=[]
+linkTable.append(3)  # 在尾部追加节点
+linkTable.append(5)
+print(linkTable)
+linkTable.insert(1,4)  # 在链表中间插入节点
+print(linkTable)
+linkTable.remove(linkTable[1])  # 删除节点
+print(linkTable)
