fbdabaf0f154b0b2b56ab3e6386e9acade3a619b
DreamerYee <1584802414@qq.com>
2019-10-08 20:23:15 +0800

    update DemoA

---
+++ python作业/python课后作业/DemoA.py
@@ -0,0 +1,25 @@
+# -*-coding:utf-8 -*-
+# *-*Author:DreamYee*-*
+# Create:2019/10/08 14:38
+# File:DemoA.py
+
+# 1.有一段英文，其中有单词连续重复了2次，编写程序检查重复的单词并只保留一个。
+# 例如，文本内容为“This is is a desk.”，程序输出为“This is a desk.”
+import re
+x = 'This is is a desk.'
+pattern = re.compile(r'\b(\w+)(\s+\1){1, }\b')
+matchResult = pattern.search(x)
+x = pattern.sub(matchResult.group(1), x)
+print(x)
+
+# pattern = re.compile(r'(?P<f>\b\w+\b)\s(?P=f)')
+# x = x.replace(matchResult.group(0),matchResult.group(1))
+
+'''
+# 2.编写程序，用户输入一段英文，然后输出这段英文中所有长度为3个字母的单词。
+import re
+x = input('Please input a string:')
+pattern = re.compile(r'\b[a-zA-Z]{3}\b')
+print(pattern.findall(x))
+'''
+
