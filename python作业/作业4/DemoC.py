0259722a48096e49515a7656514a01bc561a084c
DreamerYee <1584802414@qq.com>
2019-10-17 19:47:19 +0800

    initial commit

--- python作业/作业4/DemoC.py
+++ python作业/作业4/DemoC.py
@@ -5,3 +5,20 @@
 
 # 3.设有三个变量a,b,c,分别对三个变量赋值，并对三个变量进行排序。
 # 如a=5,b=7,c=6,则排序结果为b>c>a。
+
+a = input("请输入一个数a=：")
+b = input("请输入一个数b=：")
+c = input("请输入一个数c=：")
+if a>b>c:
+    print("a>b>c")
+elif a>c>b:
+    print("a>c>b")
+elif b>a>c:
+    print("b>a>c")
+elif b>c>a:
+    print("b>c>a")
+elif c>a>b:
+    print("c>a>b")
+elif c>b>a:
+    print("c>b>a")
+
