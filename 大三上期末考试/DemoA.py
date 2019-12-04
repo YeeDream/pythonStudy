182ab886c15e5d231f913477ed1fb3d76fd80092
DreamerYee <1584802414@qq.com>
2019-12-04 20:06:54 +0800

    initial commit

---
+++ 大三上期末考试/DemoA.py
@@ -0,0 +1,9 @@
+# -*-coding:utf-8 -*-
+# *-*Author:DreamYee*-*
+# Create:2019/12/04 16:42
+# File:DemoA.py
+
+def myfun():
+    pass
+
+myfun()
---
+++ 大三上期末考试/DemoB.py
@@ -0,0 +1,10 @@
+# -*-coding:utf-8 -*-
+# *-*Author:DreamYee*-*
+# Create:2019/12/04 16:43
+# File:DemoB.py
+
+def showNumber(numbers):
+    for n in numbers:
+        print(n)
+
+showNumber(3.4)
